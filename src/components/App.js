fcc594eb0eb24ab0b62a6a5019692f35562bf5e2
Diego Balbis <dbalbis8@gmail.com>
2022-03-15 01:30:23 -0300

    Agregada la NavBar

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Container from '@mui/material/Container';
+import '../styles/styles.scss';
+
+function App() {
+  return (
+    <main>
+      <Container fixed className="ultimosProductos">
+        <h2>Nuestros Productos</h2>
+      </Container>
+    </main>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/styles.scss';
+import NavBar from './components/NavBar';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <NavBar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
@@ -14,4 +15,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
